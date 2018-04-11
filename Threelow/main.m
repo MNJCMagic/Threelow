09920898eb48e1d47f0787962a7b13801236602c
Mike Cameron <mike@mikecameron.tv>
2018-04-11 13:49:39 -0400

    Added roll on roll functionality

--- Threelow/main.m
+++ Threelow/main.m
@@ -18,15 +18,34 @@ int main(int argc, const char * argv[]) {
         Dice *die4 = [[Dice alloc] init];
         Dice *die5 = [[Dice alloc] init];
         
-        NSLog(@"%ld, %ld, %ld, %ld, %ld", die1.value, die2.value, die3.value, die4.value, die5.value);
+        BOOL play = true;
         
-        [die1 randomizeValue];
-        [die2 randomizeValue];
-        [die3 randomizeValue];
-        [die4 randomizeValue];
-        [die5 randomizeValue];
+        while (play) {
+            
+            char inputChars[255];
+            // char num[255];
+            printf("roll to roll\n");
+            fgets(inputChars, 255, stdin);
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            NSCharacterSet *space = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+            
+            NSString *input = [inputString stringByTrimmingCharactersInSet:space];
+            
+            if ([input isEqualToString:@"roll"]) {
+                
+                
+                [die1 randomizeValue];
+                [die2 randomizeValue];
+                [die3 randomizeValue];
+                [die4 randomizeValue];
+                [die5 randomizeValue];
+                
+                NSLog(@"%ld, %ld, %ld, %ld, %ld", die1.value, die2.value, die3.value, die4.value, die5.value);
+            }
+   
+        }
         
-        NSLog(@"%ld, %ld, %ld, %ld, %ld", die1.value, die2.value, die3.value, die4.value, die5.value);
+    
         
         
         
